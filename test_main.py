fac327c5978a33eb8c98a13beb30dc682e395eba
HuyNT42 <nguyenthaihuy2802@gmail.com>
2024-03-29 15:39:02 +0700

    test main

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ import main
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+PASSWORD = 'huff-puff-123'
 
 @pytest.fixture
 def client():
